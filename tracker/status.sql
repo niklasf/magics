f8700f8003982ff5093ccc215403611e2a15d7f0
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-12-26 20:44:32 +0100

    update status.sql

--- tracker/status.sql
+++ tracker/status.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*), SUM(s0), SUM(s1), SUM(s2), SUM(s3), SUM(s4), SUM(s5), SUM(s6), SUM(s7), SUM(s8), SUM(magics) FROM prefix WHERE submitted = 4;
+SELECT COUNT(*), SUM(s0), SUM(s1), SUM(s2), SUM(s3), SUM(s4), SUM(s5), SUM(s6), SUM(s7), SUM(s8), SUM(s9), SUM(s10), SUM(s11), SUM(magics) FROM prefix WHERE submitted = 5;
