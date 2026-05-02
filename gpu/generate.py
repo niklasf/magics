24d6b84389a6c8dfe083bf1437da16e9f65d7ff0
Niklas Fiekas <niklas.fiekas@backscattering.de>
2026-05-02 12:10:57 +0200

    gpu: handle wider tables again

--- gpu/generate.py
+++ gpu/generate.py
@@ -16,9 +16,10 @@
 import chess
 import itertools
 
-DELTAS = [-7, 7, -9, 9]
-SQUARE = chess.B2
-SHIFT = 4
+PIECE_TYPE = chess.ROOK
+SQUARE = chess.H3
+SHIFT = 11
+DELTAS = [-7, 7, -9, 9] if PIECE_TYPE == chess.BISHOP else [-8, 8, -1, 1]
 
 mask = chess._sliding_attacks(SQUARE, 0, DELTAS) & ~chess._edges(SQUARE)
 
@@ -48,15 +49,11 @@ while True:
         break
 
 max_attack_id = max(refs.values())
-assert (1 << SHIFT) * SHIFT <= 64, "can pack table into uint64_t"
-assert max_attack_id < (1 << SHIFT)
-
-print("""
-__device__ bool check_magic(uint64_t magic) {{
-    uint64_t table = {zero}; // slot 0 pre-set for subset 0
-    int shift;
-    uint64_t slot;
-""".format(zero=refs[0]))
+packed = (1 << SHIFT) * SHIFT <= 64 and max_attack_id < (1 << SHIFT)
+if packed:
+    assert max_attack_id < (1 << SHIFT)
+else:
+    assert max_attack_id < 256, f"attack_id {max_attack_id} does not fit in uint8_t"
 
 by_attack = {}
 for subset, attack_id in refs.items():
@@ -66,15 +63,39 @@ for subset, attack_id in refs.items():
 # Interleave round-robin, rarest attack_id first, so consecutive checks
 # always have different attack_ids and less frequent ids are placed early.
 groups = sorted(by_attack.items(), key=lambda x: len(x[1]))
-for i in range(max(len(subsets) for _, subsets in groups)):
-    for attack_id, subsets in groups:
-        if i < len(subsets):
-            subset = subsets[i]
-            print("    shift = (int)((magic * UINT64_C({})) >> {}) * {};".format(subset, 64 - SHIFT, SHIFT))
-            print("    slot = (table >> shift) & UINT64_C(0x{:x});".format((1 << SHIFT) - 1))
-            print("    if (slot && slot != UINT64_C({})) return false;".format(attack_id))
-            print("    table |= UINT64_C({}) << shift;".format(attack_id))
-            print()
+
+if packed:
+    print("""
+__device__ bool check_magic(uint64_t magic) {{
+    uint64_t table = {zero}; // slot 0 pre-set for subset 0
+    int shift;
+    uint64_t slot;
+""".format(zero=refs[0]))
+
+    for i in range(max(len(subsets) for _, subsets in groups)):
+        for attack_id, subsets in groups:
+            if i < len(subsets):
+                subset = subsets[i]
+                print("    shift = (int)((magic * UINT64_C({})) >> {}) * {};".format(subset, 64 - SHIFT, SHIFT))
+                print("    slot = (table >> shift) & UINT64_C(0x{:x});".format((1 << SHIFT) - 1))
+                print("    if (slot && slot != UINT64_C({})) return false;".format(attack_id))
+                print("    table |= UINT64_C({}) << shift;".format(attack_id))
+                print()
+else:
+    print("""
+__device__ bool check_magic(uint64_t magic) {{
+    uint8_t table[1 << {shift}] = {{ {zero} }}; // slot 0 pre-set for subset 0
+    int idx;
+""".format(shift=SHIFT, zero=refs[0]))
+
+    for i in range(max(len(subsets) for _, subsets in groups)):
+        for attack_id, subsets in groups:
+            if i < len(subsets):
+                subset = subsets[i]
+                print("    idx = (int)((magic * UINT64_C({})) >> {});".format(subset, 64 - SHIFT))
+                print("    if (table[idx] && table[idx] != {}) return false;".format(attack_id))
+                print("    table[idx] = {};".format(attack_id))
+                print()
 
 print("""
     return true;
